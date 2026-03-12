e0af606d541cf6c6553e52edb2807deb38e6b6e1
Pranjal Poudel <pranjalpaudel62@gmail.com>
2026-03-12 19:54:33 +0545

    Remove insecure static serve fallback

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,10 +27,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    # Fallback to serve static/media files through Django/WhiteNoise when DEBUG=False
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    ]
 

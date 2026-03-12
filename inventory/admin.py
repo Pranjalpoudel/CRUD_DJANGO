87395ea17cbfeec06d39f25842cd3d5f6f3dd566
Pranjal Poudel <pranjalpaudel62@gmail.com>
2026-03-12 16:35:34 +0545

    Register Inventory models in Django Admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Item
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'icon', 'created_at')
+    search_fields = ('name',)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'status', 'price', 'updated_at')
+    list_filter = ('category', 'status', 'created_at')
+    search_fields = ('name', 'serial_number', 'description')
+    date_hierarchy = 'created_at'
